9c75e431b9f68fab197501ff21aaf9a1f102422b
Anusara-anu <81525953+ANUSARA-ANU@users.noreply.github.com>
2022-02-08 15:09:29 +0530

    Update help.js

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -22,7 +22,7 @@ module.exports = {
 
     const embed = new Discord.MessageEmbed()
     .setTitle(`${bot} Help`)
-    .setDescription(` Hello **${message.author.username}**, \n *Choose an category below to see commands* \n\n :question: New to ${bot}? Check out server \n ${support} \n\n Also Join Our Developer Server \n https://discord.gg/gU7XAxTpX5`)
+    .setDescription(` Hello **${message.author.username}**, \n *Choose an category below to see commands* \n\n :question: New to ${bot}? Check out server \n ${support} \n\n Also Join Our Developer Server \n https://discord.gg/BYpGMHbfr7`)
     .setThumbnail(client.user.displayAvatarURL())
     .setColor("RANDOM")
     .setFooter(`Requested by: ${message.author.tag}`)
@@ -91,81 +91,3 @@ module.exports = {
    
 }};
 
-
-module.exports = {
-  name: "help",
-  description: "Info",
-
-  run: async (client, message, args) => {
-
-
-    const embed = new Discord.MessageEmbed()
-    .setTitle(`${bot} Help`)
-    .setDescription(` Hello **${message.author.username}**, \n *Choose an category below to see commands* \n\n :question: New to ${bot}? Check out server \n ${support} \n\n Also Join Our Developer Server \n https://discord.gg/gU7XAxTpX5`)
-    .setThumbnail(client.user.displayAvatarURL())
-    .setColor("RANDOM")
-    .setFooter(`Requested by: ${message.author.tag}`)
-
-
-    const music = new Discord.MessageEmbed()
-    .setColor(Color)
-    .setTitle(`Music`)
-    .setThumbnail(client.user.displayAvatarURL())
-    .setDescription(`Here are all the music commands: \n\n \`join\`, \`leave\`, \`loop\`, \`nowplaying\`, \`pause\`,  \`play\`,  \`queue\`,  \`remove\`,  \`resume\`,  \`search\`,  \`skip\`,  \`skipall\`,  \`stop\`,  \`volume\``)
-    .setFooter(`Requested by: ${message.author.tag}`)
-
-
-
-    const info = new Discord.MessageEmbed()
-    .setColor(Color)
-    .setTitle(`Info`)
-    .setThumbnail(client.user.displayAvatarURL())
-    .setDescription(`Here are all the Info commands: \n\n \`help\`, \`invite\`, \`setprefix\`,  \`setpre\`,  \`removepremium\``)
-    .setFooter(`Requested by: ${message.author.tag}`)
-
-
-    let button1 = new MessageButton()
-    .setLabel(`Music`)
-    .setID(`music`)
-    .setStyle("blurple");
-    
-
-    let button2 = new MessageButton()
-    .setLabel(`Info`)
-    .setID(`info`)
-    .setStyle("green");
-
-
-
-    let row = new MessageActionRow()
-    .addComponents(button1, button2);
-
-
-
-    const MESSAGE = await message.channel.send(embed, row);
-
-    const filter = ( button ) => button.clicker.user.id === message.author.id 
-    const collector = MESSAGE.createButtonCollector(filter, { time : 300000 });
-
-    collector.on('collect', async (b) => {
-
-        if(b.id == "music") {
-
-            MESSAGE.edit(music, row);
-            await b.reply.defer()
-            
-        }
-
-         if(b.id == "info") {
-
-            MESSAGE.edit(info, row);
-            await b.reply.defer()
-            
-        }
-
-
-    });
-
-
-   
-}};
